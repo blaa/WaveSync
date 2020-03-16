e65cd39c17953df9402604bdf3c5187ff539ba76
Tomasz bla Fortuna <bla@thera.be>
2020-03-16 01:04:15 +0100

    WIP: Almost behaves OK.

--- libwavesync/audio_output.py
+++ libwavesync/audio_output.py
@@ -1,3 +1,5 @@
+from libwavesync import time_machine
+
 class AudioOutput:
     """
     Output abstraction - wraps all methods of sound card required to work.
@@ -14,7 +16,7 @@ class AudioOutput:
         # Generate silence frames (zeroed) of appropriate sizes for chunks
         self.silence_cache = None
 
-        self.chunk_frames = config.chunk_size / config.frame_size
+        self.chunk_frames = config.chunk_size // config.frame_size
 
         if device_index == -1:
             # We are tested. Don't open stream (stop at calculation of chunk_frames).
@@ -40,9 +42,10 @@ class AudioOutput:
                                         frames_per_buffer=buffer_size,
                                         output_device_index=device_index)
 
-        self.max_buffer = self.get_write_available()
+        # It seems to be twice the size we set, independently on the number of channels
+        self.buffer_total = self.get_write_available()
 
-        print("BUFS", buffer_size, self.max_buffer) # max_buffer seems twice the size; mono/stereo?
+        print("BUFS", config.channels, buffer_size, self.buffer_total) # max_buffer seems twice the size; mono/stereo?
         print("CONFIG", config, config.chunk_time)
 
     def __del__(self):
@@ -56,16 +59,49 @@ class AudioOutput:
             self.pyaudio = None
 
     def get_write_available(self):
+        "Return number of whole frames we can write to the buffer"
         return self.stream.get_write_available()
 
     def write(self, data):
         return self.stream.write(data)
 
-    def get_silent_chunk(self):
-        "Generate and cache silent chunks"
+    def can_write_chunk(self):
+        "Can we write at least a single chunk to the buffer without blocking?"
+        return self.stream.get_write_available() >= self.chunk_frames
+
+    def get_play_time(self, available, now):
+        "What is the current play time of the chunk we would schedule now?"
+        buffer_delay = (self.buffer_total - available) / self.config.rate
+        return now + buffer_delay + self.config.sink_latency_s
+
+    def play_silence(self, seconds):
+        """
+        Play silence
+        TODO: Parametrized
+        """
+        chunk = self.get_silence()
+        times = int(seconds * 1000)
+        print("Playing silence", seconds * 1000, "ms")
+        self.write(chunk * times)
+
+        # Return number of frames written to the buffer
+        return len(chunk) // self.config.frame_size * times
+
+    def get_silence(self):
+        "Generate and cache a 1ms silence chunk"
         if self.silence_cache is not None:
             return self.silence_cache
 
-        silent_chunk = b'\x00' * self.config.chunk_size
+        frame = (
+            b'\x00\x00\x00'
+            if self.config.sample == 24
+            else b'\x00\x00'
+        )
+        if self.config.channels == 2:
+            frame = frame * 2
+
+        frames = self.config.rate // 1000 + 1
+
+        silent_chunk = frame * frames
         self.silence_cache = silent_chunk
         return silent_chunk
--- libwavesync/chunk_player.py
+++ libwavesync/chunk_player.py
@@ -19,15 +19,13 @@ class ChunkPlayer:
 
         # Configuration
         self.tolerance_ms = tolerance_ms
+        self.tolerance_s = tolerance_ms / 1000.0
 
         # Audio state
         self.buffer_size = buffer_size
         self.device_index = device_index
         self.audio_output = None
         self.max_delay = 5
-
-        # Number of silent frames that need to be inserted to get in sync
-        self.silence_to_insert = 0
 
         # Used to quit main loop
         self.stop = False
@@ -37,9 +35,8 @@ class ChunkPlayer:
 
     def clear_state(self):
         "Clear player queue"
-        self.silence_to_insert = 0
 
-        # Clear the chunk list, but preserve CFG commands
+        # Clear the chunk list, but preserve CFG command
         cfg = None
         for cmd, item in self.chunk_queue.chunk_list:
             if cmd == self.chunk_queue.CMD_CFG:
@@ -59,7 +56,8 @@ class ChunkPlayer:
             self.clear_state()
         else:
             # Just slowly resync
-            self.silence_to_insert += item
+            # NOTE: There was silence insertion here, now, it should be automatic.
+            pass
 
     def _handle_cmd_cfg(self, audio_config):
         "Handle configuration command"
@@ -72,89 +70,134 @@ class ChunkPlayer:
                           self.audio_output.config.latency_ms) / 1000
         print("Assuming maximum chunk delay of %.2fms in this setup" % (self.max_delay * 1000))
 
-    async def _handle_empty_queue(self):
-        "Handle case with the empty input queue"
-        if self.audio_output is not None:
-            print("Queue empty - waiting")
+    # async def _handle_empty_queue(self):
+    #     "Handle case with the empty input queue"
+    #     if self.audio_output is not None:
+    #         print("Queue empty - waiting")
 
-        self.chunk_queue.chunk_available.clear()
-        # FIXME: This blocks. But instead we should be pumping data into output buffer.
-        await self.chunk_queue.chunk_available.wait()
+    #     self.chunk_queue.chunk_available.clear()
+    #     # FIXME: This blocks. But instead we should be pumping data into output buffer.
+    #     await self.chunk_queue.chunk_available.wait()
 
-        if self.audio_output is not None:
-            await asyncio.sleep(self.audio_output.config.latency_ms / 1000 / 4)
-            print("Got stream flowing. q_len=%d" % len(self.chunk_queue.chunk_list))
+    #     if self.audio_output is not None:
+    #         await asyncio.sleep(self.audio_output.config.latency_ms / 1000 / 4)
+    #         print("Got stream flowing. q_len=%d" % len(self.chunk_queue.chunk_list))
 
-    async def _handle_cmd_audio(self, item):
-        "Handle chunk playback"
-        mid_tolerance_s = self.tolerance_ms / 2 / 1000
-        one_ms = 1/1000.0
+    # async def _handle_cmd_audio(self, item):
+    #     "Handle chunk playback"
+    #     mid_tolerance_s = self.tolerance_ms / 2 / 1000
+    #     one_ms = 1/1000.0
 
-        mark, chunk = item
-        desired_time = mark - self.audio_output.config.sink_latency_s
+    #     mark, chunk = item
+    #     desired_time = mark - self.audio_output.config.sink_latency_s
 
-        # 0) We got the next chunk to be played
-        now = time_machine.now()
+    #     # Calculate latency caused by buffer
+    #     a = self.audio_output.get_write_available()
+    #     # Assuming this is in full "stereo frames"
+    #     in_buffer = (self.audio_output.buffer_total - a)
+    #     buffer_delay = in_buffer / self.audio_output.config.rate
+    #     desired_time -= buffer_delay
 
-        # Negative when we're lagging behind.
-        delay = desired_time - now
+    #     # 0) We got the next chunk to be played
+    #     now = time_machine.now()
 
-        self.stats.total_delay += delay
-        self.stats.total_chunks += 1
+    #     # Negative when we're lagging behind.
+    #     delay = desired_time - now
 
-        # Probabilistic drop of lagging chunks to get back on track.
-        # Probability of drop is higher, the more chunk lags behind current
-        # time. Similar to the RED algorithm in TCP congestion.
-        if delay < -mid_tolerance_s:
-            over = -delay - mid_tolerance_s
-            prob = over / mid_tolerance_s
-            if random.random() < prob:
-                s = "Drop chunk: q_len=%2d delay=%.1fms < 0. tolerance=%.1fms: P=%.2f"
-                s = s % (len(self.chunk_queue.chunk_list),
-                         delay * 1000, self.tolerance_ms, prob)
-                print(s)
-                self.stats.time_drops += 1
-                return
+    #     self.stats.total_delay += delay
+    #     self.stats.total_chunks += 1
 
-        elif delay > self.max_delay:
-            # Probably we hanged for so long time that the time recovering
-            # mechanism rolled over. Recover
-            print("Huge recovery - delay of %.2f exceeds the max delay of %.2f" % (
-                delay, self.max_delay))
-            self.clear_state()
-            return
+    #     # TODO: Simplify and remove the max_delay maybe?
+    #     if delay > self.max_delay:
+    #         # Probably we hanged for so long time that the time recovering
+    #         # mechanism rolled over. Recover
+    #         print("Huge recovery - delay of %.2f exceeds the max delay of %.2f" % (
+    #             delay, self.max_delay))
+    #         self.clear_state()
+    #         return
 
-        # If chunk is in the future - wait until it's within the tolerance
-        elif delay > one_ms:
-            to_wait = max(one_ms, delay - one_ms)
-            await asyncio.sleep(to_wait)
+    #     # If chunk is in the future - wait until it's within the tolerance
+    #     elif delay > one_ms:
+    #         to_wait = max(one_ms, delay - one_ms)
+    #         # FIXME: With empty buffer this should be inputting SILENCE
+    #         await asyncio.sleep(to_wait)
 
-        # Wait until we can write chunk into output buffer. This might
-        # delay us too much - the probabilistic dropping mechanism will kick
-        # in.
-        times = 0
-        while True:
-            buffer_space = self.audio_output.get_write_available()
-            if buffer_space < self.audio_output.chunk_frames:
-                self.stats.output_delays += 1
-                await asyncio.sleep(one_ms)
-                times += 1
-                if times > 200:
-                    print("Hey, the output is STUCK!")
-                    await asyncio.sleep(1)
-                    break
-                continue
-            self.audio_output.write(chunk)
-            return
+    #     # Wait until we can write chunk into output buffer. This might
+    #     # delay us too much - the probabilistic dropping mechanism will kick
+    #     # in.
+    #     times = 0
+    #     while True:
+    #         buffer_space = self.audio_output.get_write_available()
+    #         if buffer_space < self.audio_output.chunk_frames:
+    #             # Not enough space in buffer to add chunk.
 
-    async def chunk_player(self):
-        "Reads asynchronously chunks from the list and plays them"
+    #             # Probabilistic drop of lagging chunks to get back on track.
+    #             # Probability of drop is higher, the more chunk lags behind current
+    #             # time. Similar to the RED algorithm in TCP congestion.
+    #             if delay < -mid_tolerance_s:
+    #                 over = -delay - mid_tolerance_s
+    #                 prob = over / mid_tolerance_s
+    #                 if random.random() < prob:
+    #                     s = "Drop chunk: q_len=%2d delay=%.1fms < 0. tolerance=%.1fms: P=%.2f"
+    #                     s = s % (len(self.chunk_queue.chunk_list),
+    #                              delay * 1000, self.tolerance_ms, prob)
+    #                     print(s)
+    #                     self.stats.time_drops += 1
+    #                     return
 
-        while not self.stop:
-            if not self.chunk_queue.chunk_list:
-                await self._handle_empty_queue()
-                continue
+    #             self.stats.output_delays += 1
+    #             await asyncio.sleep(10 * one_ms)
+    #             times += 1
+    #             if times > 200:
+    #                 print("Hey, the output is STUCK!")
+    #                 await asyncio.sleep(1.0)
+    #                 break
+    #             continue
+    #         self.audio_output.write(chunk)
+    #         return
 
+    # async def chunk_player_orig(self):
+    #     "Reads asynchronously chunks from the list and plays them"
+
+    #     while not self.stop:
+    #         if not self.chunk_queue.chunk_list:
+    #             await self._handle_empty_queue()
+    #             continue
+
+    #         cmd, item = self.chunk_queue.chunk_list.popleft()
+
+    #         if cmd == self.chunk_queue.CMD_CFG:
+    #             self._handle_cmd_cfg(item)
+    #             continue
+
+    #         if cmd == self.chunk_queue.CMD_DROPS:
+    #             self._handle_cmd_drops(item)
+    #             continue
+
+    #         # CMD_AUDIO
+
+    #         if self.audio_output is None:
+    #             # No output, no playing.
+    #             continue
+
+    #         await self._handle_cmd_audio(item)
+
+    #         # Main status line
+    #         self.stats.chunk(queue_length=len(self.chunk_queue.chunk_list))
+
+    #     print("- Finishing chunk player")
+
+
+    async def read_commands(self, block=False):
+        """
+        Read next entry from chunk_list, handle transparently all non-audio
+        commands and return a next audio chunk if available.
+        """
+        if block and not self.chunk_queue.chunk_list:
+            self.chunk_queue.chunk_available.clear()
+            await self.chunk_queue.chunk_available.wait()
+
+        while self.chunk_queue.chunk_list:
             cmd, item = self.chunk_queue.chunk_list.popleft()
 
             if cmd == self.chunk_queue.CMD_CFG:
@@ -165,15 +208,94 @@ class ChunkPlayer:
                 self._handle_cmd_drops(item)
                 continue
 
-            # CMD_AUDIO
+            if cmd == self.chunk_queue.CMD_AUDIO:
+                self.stats.total_chunks += 1
 
-            if self.audio_output is None:
-                # No output, no playing.
-                continue
+                # Main status line
+                self.stats.chunk(queue_length=len(self.chunk_queue.chunk_list))
 
-            await self._handle_cmd_audio(item)
+                return item
 
-            # Main status line
-            self.stats.chunk(queue_length=len(self.chunk_queue.chunk_list))
+            assert False, "Invalid command received"
 
+        # No audio in queue
+        return None
+
+    async def chunk_player(self):
+        "Reads asynchronously chunks from the list and plays them"
+        # Initially we wait for configuration
+        while not self.stop:
+            await self.read_commands(block=True)
+            if self.audio_output:
+                break
+
+        # Audio output is enabled and we have a stream configuration,
+        # it can change though later.
+        i = 0
+        while not self.stop:
+            # In general case we have a chunk to add to the output buffer every
+            # `chunk_time` seconds.
+
+            # New idea 1:
+            # 1) Wait until we can write at least one chunk
+            # 2) Read chunks until first with correct tolerances
+            # 3) Fill buffer with chunks (what if some are missing?)
+            # 4) When buffer is full we have time to probably drop some more chunks
+
+            # New idea 2:
+            # - Detach stream control from chunks. Observe average delay over time
+            #   and adjust time by requesting drop/silence injection
+            # - During initial audio start try to sync though.
+            # - This can allow PID like approach to synchro.
+
+            # Feed output buffer with as many chunks as you can.
+
+            # Save time and buffer status at the same moment
+            buffer_space = self.audio_output.get_write_available()
+            now = time_machine.now()
+
+            while True:
+                if buffer_space < self.audio_output.chunk_frames:
+                    # Too little space for a single chunk - we're done
+                    break
+
+                # We either play silence, or some chunk.
+                item = await self.read_commands()
+                if item is None:
+                    print("No commands - Silence.")
+                    frames = self.audio_output.play_silence(2 * self.audio_output.config.chunk_time)
+                    buffer_space -= frames
+                    continue
+
+                mark, chunk = item
+                i += 1
+
+                play_time = self.audio_output.get_play_time(buffer_space, now)
+                delay = play_time - mark
+                # chunk delay > 0 -> too late
+                # chunk delay < 0 -> too early
+
+                if delay > self.tolerance_s:
+                    # Too late - DROP chunk
+                    self.stats.time_drops += 1
+                    print("DROP chunk={} delay={:.2f}ms tol={:.2f}ms bufspace={} ".format(
+                        i,
+                        delay*1000, self.tolerance_s*1000, buffer_space))
+                    continue
+                if delay < -self.tolerance_s:
+                    # Too early - Wait while generating silence
+                    silence = min(-delay - self.tolerance_s / 2, 20 / 1000)
+
+                    frames = self.audio_output.play_silence(silence)
+                    print("WAIT WITH SILENCE delay={:.2f}ms, frames={}/space={} silence={:.2f}ms".format(delay*1000, frames, buffer_space, silence*1000))
+                    buffer_space -= frames
+                    # BUG FIXME: This might've blocked! TODO: WE SHOULD WAIT WITH A LOOP!
+                    # await asyncio.sleep(silence)
+                print("PLAY cnt={} buff_space={} delay={:.2f}".format(
+                    i, buffer_space, delay*100))
+                self.audio_output.write(chunk)
+                buffer_space -= len(chunk) // self.audio_output.config.frame_size
+
+            print("Waiting for output buffer", buffer_space)
+            await asyncio.sleep(6.0 * self.audio_output.config.chunk_time)
         print("- Finishing chunk player")
